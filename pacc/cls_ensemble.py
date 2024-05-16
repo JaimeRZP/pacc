e9bcba80861ef6558db915afdcc6db854f3cd633
jaimerzp <jaimerz011235813@gmal.com>
2024-05-16 12:05:14 +0100

    covs working

--- pacc/cls_ensemble.py
+++ pacc/cls_ensemble.py
@@ -6,17 +6,39 @@ class ClsEnsemble(object):
     def __init__(self, s, y):
 
         # Apply scale cuts
-        self._apply_scale_cuts(s, y)
+        self.order = y["order"]
+        self.s = self._cut(s)
+        self._get_defaults()
 
+    def _cut(self, s):
+        indices = np.array([])
+        print(self.order)
+        for cl in self.order:
+            t1, t2 = cl["tracers"]
+            cls = cl["cls"]
+            for cl_name in cls:
+                if "ell_cuts" in list(cl.keys()):
+                    lmin, lmax = cl["ell_cuts"]
+                    ind = s.indices(cl_name, (t1, t2),
+                                ell__gt=lmin, ell__lt=lmax)
+                else:
+                    ind = s.indices(cl_name, (t1, t2))
+                indices = np.append(indices, ind)
+        if len(indices) != 0:
+            indices = indices.astype(int)
+            s.keep_indices(indices)
+        return s
+
+    def _get_defaults(self):
         self.indices = []
         self.data = []
         self.ls = []
         self.pairs = []
-        for cl in y["order"]:
+        for cl in self.order:
             t1, t2 = cl["tracers"]
             cls = cl["cls"]
             for cl_name in cls:
-                l, c_ell, ind = s.get_ell_cl(
+                l, c_ell, ind = self.s.get_ell_cl(
                     cl_name, t1, t2,
                     return_cov=False,
                     return_ind=True)
@@ -25,29 +47,13 @@ class ClsEnsemble(object):
                 self.ls.append(l)
                 self.pairs.append([t1, t2])
         
-        self.cov = s.covariance.dense
-        lengths = [len(l) for l in self.ls]
-        self.edges  = np.append(np.array([0]), np.cumsum(lengths))
+        self.cov = self.s.covariance.dense
         errs = np.sqrt(np.diag(self.cov))
         self.errs = [errs[ind] for ind in self.indices]
 
-    def _apply_scale_cuts(self, s, y):
-        indices = np.array([])
-        for cl in y["order"]:
-            t1, t2 = cl["tracers"]
-            cls = cl["cls"]
-            if "ell_cuts" in list(cl.keys()):
-                lmin, lmax = cl["ell_cuts"]
-                for cl_name in cls:
-                    ind = s.indices(cl_name, (t1, t2),
-                                    ell__gt=lmin, ell__lt=lmax)
-                    indices = np.append(indices, ind)
-        if len(indices) != 0:
-            indices = indices.astype(int)
-            s.keep_indices(indices)
-
-    def _segment(self, A):
-        seg_A = []
-        for i in range(0, len(self.edges)-1):
-            seg_A.append(A[self.edges[i]:self.edges[i+1]])
-        return seg_A
+class ClSubSemble(ClsEnsemble):
+        def __init__(self, cl_ensemble, pairs):
+            poss = [cl_ensemble.pairs.index(pair) for pair in pairs]
+            self.order  = [cl_ensemble.order[pos] for pos in poss]
+            self.s = self._cut(cl_ensemble.s)
+            self._get_defaults()
