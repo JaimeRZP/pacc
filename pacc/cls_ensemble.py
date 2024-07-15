b3ee29c622eba3992697212258662627bdad3cad
jaimerzp <jaimerz011235813@gmal.com>
2024-07-15 16:13:56 +0100

    now also with real space functions

--- pacc/cls_ensemble.py
+++ pacc/cls_ensemble.py
@@ -3,10 +3,13 @@ import sacc
 import yaml
 
 class ClsEnsemble(object):
-    def __init__(self, s, y, label=None):
+    def __init__(self, s, y=None, label=None):
 
         # Apply scale cuts
-        self.order = y["order"]
+        if y is not None:
+            self.order = y["order"]
+        else:
+            self.order = self._make_order(s)
         self.s = self._cut(s)
         self.label = label
         self._get_defaults()
@@ -38,18 +41,40 @@ class ClsEnsemble(object):
             t1, t2 = cl["tracers"]
             cls = cl["cls"]
             for cl_name in cls:
-                l, c_ell, ind = self.s.get_ell_cl(
-                    cl_name, t1, t2,
-                    return_cov=False,
-                    return_ind=True)
-                self.indices.append(ind)
-                self.data.append(c_ell)
-                self.ls.append(l)
-                self.pairs.append([t1, t2])
+                # Figure out if we are dealing with a cl or xi
+                if "cl" in cl_name:
+                    l, c_ell, ind = self.s.get_ell_cl(
+                        cl_name, t1, t2,
+                        return_cov=False,
+                        return_ind=True)
+                    self.indices.append(ind)
+                    self.data.append(c_ell)
+                    self.ls.append(l)
+                    self.pairs.append([t1, t2])
+                if "xi" in cl_name:
+                    theta, xi, ind = self.s.get_theta_xi(
+                        cl_name, t1, t2,
+                        return_cov=False,
+                        return_ind=True)
+                    self.indices.append(ind)
+                    self.data.append(xi)
+                    self.ls.append(theta)
+                    self.pairs.append([t1, t2])
 
         self.cov = self.s.covariance.dense
         errs = np.sqrt(np.diag(self.cov))
         self.errs = [errs[ind] for ind in self.indices]
+
+    def _make_order(self, s):
+        order = []
+        for dt in s.get_data_types():
+            ts_combs = s.get_tracer_combinations(dt)
+            for comb in ts_combs:
+                t1, t2 = comb
+                order.append({
+                    "tracers": [t1, t2],
+                    "cls": [dt]})
+        return order
 
 class ClSubSemble(ClsEnsemble):
     def __init__(self, cl_ensemble, pairs):
